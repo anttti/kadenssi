97948fb4854f3b4f5807b1b0e3f3392425a0b437
Antti Mattila <me@rarelyneeded.com>
2020-02-15 14:16:58 +0200

    Initial idea of the state machine & boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Machine, assign } from "xstate";
+import { useMachine } from "@xstate/react";
+
+interface IKadenssiSchema {
+  states: {
+    setup: {};
+    running: {};
+    paused: {};
+    finished: {};
+  };
+}
+
+type KadenssiEvent =
+  | { type: "SETUP" }
+  | { type: "ADD_STEP"; title: string; duration: number }
+  | { type: "REMOVE_STEP"; id: number }
+  | { type: "EDIT_STEP"; id: number; title: string; duration: number }
+  | { type: "MOVE_STEP_UP"; id: number }
+  | { type: "MOVE_STEP_DOWN"; id: number }
+  | { type: "RUN" }
+  | { type: "TICK" }
+  | { type: "PAUSE" }
+  | { type: "STOP" }
+  | { type: "FINISH" };
+
+interface IStep {
+  id: number;
+  title: string;
+  duration: number;
+}
+
+interface IKadenssiContext {
+  steps: IStep[];
+  currentStep: number;
+  currentTime: number;
+}
+
+const machine = Machine<IKadenssiContext, IKadenssiSchema, KadenssiEvent>(
+  {
+    id: "kadenssi",
+    initial: "setup",
+    context: {
+      steps: [],
+      currentTime: 0,
+      currentStep: 0
+    },
+    states: {
+      setup: {
+        entry: assign({
+          currentStep: _ => 0,
+          currentTime: _ => 0
+        }),
+        on: {
+          RUN: "running",
+          ADD_STEP: {
+            target: "setup",
+            actions: assign({
+              steps: (context, { title, duration }) => {
+                const id = new Date().getTime();
+                return context.steps.concat({ id, title, duration });
+              }
+            })
+          },
+          REMOVE_STEP: {
+            target: "setup",
+            actions: assign({
+              steps: (context, { id }) => context.steps.filter(s => s.id !== id)
+            })
+          },
+          EDIT_STEP: {
+            target: "setup",
+            actions: assign({
+              steps: (context, { id, title, duration }) => {
+                // @TODO: Support editing steps
+                return context.steps;
+              }
+            })
+          },
+          // @TODO: Support reordering
+          MOVE_STEP_UP: "setup",
+          MOVE_STEP_DOWN: "setup"
+        }
+      },
+      running: {
+        on: {
+          PAUSE: "paused",
+          STOP: "setup",
+          TICK: {
+            target: "running",
+            actions: assign({
+              // TODO: If time is up, go to next step
+              //       If no steps left, go to finished
+              currentTime: context => context.currentTime + 1
+            })
+          },
+          FINISH: "finished"
+        }
+      },
+      paused: {
+        on: { RUN: "running", STOP: "setup" }
+      },
+      finished: {
+        on: {}
+      }
+    }
+  },
+  {
+    actions: {
+      reset: (context, event) => {}
+    }
+  }
+);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
